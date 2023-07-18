90d47ea42af80ce466408a3e424c4d8956328d14
Fabrice Deshayes aka Xtream <Fabrice-Deshayes-aka-Xtream@users.noreply.github.com>
2023-07-18 16:59:27 +0200

    add "Press any key to continue" to prevent windows closing when launch from explorer

--- run-count.ps1
+++ run-count.ps1
@@ -2,3 +2,5 @@ Write-Host "activate conda dedicated environnent" -foregroundcolor "green"
 conda activate srt-translator
 Write-Host "execute srt-count-chars" -foregroundcolor "green"
 python srt-count-chars.py
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
--- run.ps1
+++ run.ps1
@@ -2,3 +2,5 @@ Write-Host "activate conda dedicated environnent" -foregroundcolor "green"
 conda activate srt-translator
 Write-Host "execute srt-translator" -foregroundcolor "green"
 python srt-translator.py
+Write-Host -NoNewLine 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
