6b76f564a7e2b155bcfc275eda8288b530460969
Fabrice Deshayes aka Xtream <Fabrice-Deshayes-aka-Xtream@users.noreply.github.com>
2023-08-08 10:01:20 +0200

    allow multiple apikeys in deepl_apikey.txt file (comment unused with #)

--- deepl_translator.py
+++ deepl_translator.py
@@ -11,16 +11,22 @@ def init():
     global translator
 
     path_to_deepl_apikey = "deepl_apikey.txt"
+    auth_key = ''
 
     if os.path.isfile(path_to_deepl_apikey):
-        with open(path_to_deepl_apikey, "r") as apikey:
-            auth_key = apikey.readlines().pop()
+        with open(path_to_deepl_apikey, "r") as apikey_file:
+            apikeys = apikey_file.readlines()
+            for apikey in apikeys:
+                apikey = apikey.strip()
+                if apikey.startswith("#") or len(apikey) == 0:
+                    continue
+                auth_key = apikey
 
     if len(auth_key) == 0:
         auth_key = input("Enter your deepL api key: ")
-        with open(path_to_deepl_apikey, "w") as apikey:
-            apikey.write(auth_key)
-            apikey.close()
+        with open(path_to_deepl_apikey, "a") as apikey_file:
+            apikey_file.write('\n' + auth_key)
+            apikey_file.close()
 
     # init deepl translator
     translator = deepl.Translator(auth_key)
@@ -31,5 +37,10 @@ def translate_text(text_to_translate):
     if not init_done:
         init()
 
-    return translator.translate_text(text_to_translate, target_lang=config.targetLang, formality=config.formality, tag_handling=config.tag_handling,
-                                     split_sentences='nonewlines').text
+    return translator.translate_text(
+        text_to_translate,
+        target_lang=config.targetLang,
+        formality=config.formality,
+        tag_handling=config.tag_handling,
+        split_sentences='nonewlines'
+    ).text
